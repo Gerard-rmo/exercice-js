0fa33c869e09ccdd6e513f4c24c81c0408b77426
Gerard-rmo <gerardromeroglaive@proton.me>
2024-11-07 15:21:13 +0100

    Add files via upload

---
+++ 002-nombres/script.js
@@ -0,0 +1,23 @@
+// exercice 0
+console.log(28 + 6);
+
+// exercice 1
+
+console.log((6 * 4 + 2) / 2);
+
+// exercice 2
+
+console.log((1337 + 256) * 2);
+
+// exercice 3
+
+console.log((1337 + 256) * 1.5 + 500);
+
+// exercice 4
+
+console.log((35 - 23) / 2);
+console.log(6 * 3);
+
+//& bogue 0
+
+console.log(2350 * 0.88 + 1000);
