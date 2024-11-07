8b7fa19b4b517d63c04da92fd23a14190f6c6ec7
Gerard-rmo <gerardromeroglaive@proton.me>
2024-11-07 15:34:19 +0100

    Add files via upload

---
+++ 003-boléens/script.js
@@ -0,0 +1,14 @@
+// exercice 0
+
+console.log(65 < 70 && 4 > 3);
+
+// exercice 2
+
+console.log(140 < 150 && 16 > 14);
+
+// exercice 3
+
+console.log((10 < 12 && 110 < 120) || (58 > 40 && 58 < 60));
+
+// bogue 0
+console.log(150 >= 120 || (16 > 15 && 95 < 90));
