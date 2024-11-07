f48fef25d0908921455ccbc60643e50db739e1b7
Gerard-rmo <gerardromeroglaive@proton.me>
2024-11-07 15:20:39 +0100

    Add files via upload

---
+++ 004-variables/script.js
@@ -0,0 +1,26 @@
+// bogue v0
+
+let nomParc = "Le Royaume Magique";
+console.log("Bienvenue au parc d'attractions " + nomParc);
+
+// Bogue 1
+
+let clientsHier = 359;
+let clientsAujourdhui = 374;
+let totalClients = clientsHier + clientsAujourdhui;
+console.log(totalClients);
+
+// bogue 2
+
+let nombreDeCases = 64;
+let Case = 2;
+let superficie = nombreDeCases * Case;
+console.log("Superficie du plateau: " + superficie + " m2");
+
+// bogue 3
+
+let H = 13;
+let M = 37;
+let separateur = "h";
+let horaire = H + separateur + M;
+console.log("Bienvenue, il est " + horaire);
